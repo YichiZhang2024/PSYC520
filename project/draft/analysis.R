a6ee91b1556ca21ad37830430172b277f508e394
Yichizhang2024 <yzhang97@usc.edu>
2023-05-03 09:32:32 -0700

    update results section

--- project/draft/analysis.R
+++ project/draft/analysis.R
@@ -42,30 +42,6 @@ data2 <- cbind(data2, "sex" = HolzingerSwineford1939[157:301,]$sex)
 data2_fe <- data2 %>% filter(sex == 1)
 data2_ma <- data2 %>% filter(sex == 2)
 
-## alignment on dataset with leverage points
-# mod2 <- 'f1 =~ X1 + X2 + X3'
-# mod_fit_g1 <- cfa(mod2,
-#                   sample.cov = cov(data2_fe[1:3]),
-#                   sample.mean = colMeans(data2_fe[1:3]),
-#                   sample.nobs = nrow(data2_fe),
-#                   std.lv = TRUE)
-# mod_fit_g2 <- cfa(mod2,
-#                   sample.cov = cov(data2_ma[1:3]),
-#                   sample.mean = colMeans(data2_fe[1:3]),
-#                   sample.nobs = nrow(data2_ma),
-#                   std.lv = TRUE)
-# ld <- rbind(t(lavInspect(mod_fit_g1, what = "est")$lambda),
-#             t(lavInspect(mod_fit_g2, what = "est")$lambda))
-# rownames(ld) <- c("Female", "Male")
-# nu <-  rbind(t(lavInspect(mod_fit_g1, what = "est")$nu),
-#              t(lavInspect(mod_fit_g2, what = "est")$nu))
-# rownames(nu) <- c("Female", "Male")
-# res_adlp <- invariance.alignment(
-#     lambda = ld,
-#     nu = nu,
-#     wgt = matrix(sqrt(c(nrow(data2_fe), nrow(data2_ma))), nrow = 2, ncol = 3)
-# )
-
 set.seed(3456)
 res_mve <- robalign(method = "mve_mah", data_g1 = data2_fe[,1:3], data_g2 = data2_ma[,1:3], mod = mod2, group_name = c("female", "male"))
 res_mcd <- robalign(method = "mcd_mah", data_g1 = data2_fe[,1:3], data_g2 = data2_ma[,1:3],
@@ -74,6 +50,29 @@ res_pro_mve <- robalign(method = "pro_mve", data_g1 = data2_fe[,1:3], data_g2 = 
 res_pro_mcd <- robalign(method = "pro_mcd", data_g1 = data2_fe[,1:3], data_g2 = data2_ma[,1:3], mod = mod2, group_name = c("female", "male"))
 res_outmve <- robalign(method = "outpro_mve", data_g1 = data2_fe[,1:3], data_g2 = data2_ma[,1:3], mod = mod2, group_name = c("female", "male"))
 res_outmcd <- robalign(method = "outpro_mcd", data_g1 = data2_fe[,1:3], data_g2 = data2_ma[,1:3], mod = mod2, group_name = c("female", "male"))
+## alignment on dataset with leverage points
+mod2 <- 'f1 =~ X1 + X2 + X3'
+mod_fit_g1 <- cfa(mod2,
+                  sample.cov = cov(data2_fe[1:3]),
+                  sample.mean = colMeans(data2_fe[1:3]),
+                  sample.nobs = nrow(data2_fe),
+                  std.lv = TRUE)
+mod_fit_g2 <- cfa(mod2,
+                  sample.cov = cov(data2_ma[1:3]),
+                  sample.mean = colMeans(data2_fe[1:3]),
+                  sample.nobs = nrow(data2_ma),
+                  std.lv = TRUE)
+ld <- rbind(t(lavInspect(mod_fit_g1, what = "est")$lambda),
+            t(lavInspect(mod_fit_g2, what = "est")$lambda))
+rownames(ld) <- c("Female", "Male")
+nu <-  rbind(t(lavInspect(mod_fit_g1, what = "est")$nu),
+             t(lavInspect(mod_fit_g2, what = "est")$nu))
+rownames(nu) <- c("Female", "Male")
+res_adlp <- invariance.alignment(
+    lambda = ld,
+    nu = nu,
+    wgt = matrix(sqrt(c(nrow(data2_fe), nrow(data2_ma))), nrow = 2, ncol = 3)
+)
 
 ## covariance table
 cov_mve <- as.data.frame(res_mve$rob_est_g1$weighted.covariance + res_mve$rob_est_g2$weighted.covariance)/2
@@ -82,28 +81,29 @@ cov_outmve <- as.data.frame(res_outmve$rob_est_g1$weighted.covariance + res_outm
 cov_outmcd <- as.data.frame(res_outmcd$rob_est_g1$weighted.covariance + res_outmcd$rob_est_g2$weighted.covariance)/2
 cov_promve <- as.data.frame(res_pro_mve$rob_est_g1$weighted.covariance + res_pro_mve$rob_est_g2$weighted.covariance)/2
 cov_promcd <- as.data.frame(res_pro_mcd$rob_est_g1$weighted.covariance + res_pro_mcd$rob_est_g2$weighted.covariance)/2
-cov_tab <- cbind(cov(data_gp[1:3]), cov_mve, cov_mcd, cov_outmve, cov_outmcd, cov_promve, cov_promcd)
+cov_tab <- cbind(cov(data_gp[1:3]), cov(data2[1:3]), cov_mve, cov_mcd, cov_outmve, cov_outmcd, cov_promve, cov_promcd)
 rownames(cov_tab) <- c("x1", "x2", "x3")
-colnames(cov_tab) <- rep(paste0("x", 1:3), 7)
+colnames(cov_tab) <- rep(paste0("x", 1:3), 8)
 
 ## factor loadings
-ld_tab <- cbind(res1$lambda.aligned,
+ld_tab <- cbind(res1$lambda.aligned, res_adlp$lambda.aligned,
                 res_mve$align_res$lambda.aligned, res_mcd$align_res$lambda.aligned,
-                res_pro_mve$align_res$lambda.aligned, res_pro_mcd$align_res$lambda.aligned,
-                res_outmve$align_res$lambda.aligned, res_outmcd$align_res$lambda.aligned)
+                res_outmve$align_res$lambda.aligned, res_outmcd$align_res$lambda.aligned,
+                res_pro_mve$align_res$lambda.aligned, res_pro_mcd$align_res$lambda.aligned)
 rownames(ld_tab) <- c("Female", "Male")
-colnames(ld_tab) <- rep(paste0("x", 1:3), 7)
+colnames(ld_tab) <- rep(paste0("x", 1:3), 8)
 ## intercepts
-int_tab <- cbind(res1$nu.aligned,
+int_tab <- cbind(res1$nu.aligned, res_adlp$nu.aligned,
                  res_mve$align_res$nu.aligned, res_mcd$align_res$nu.aligned,
-                 res_pro_mve$align_res$nu.aligned, res_pro_mcd$align_res$nu.aligned,
-                 res_outmve$align_res$nu.aligned, res_outmcd$align_res$nu.aligned)
+                 res_outmve$align_res$nu.aligned, res_outmcd$align_res$nu.aligned,
+                 res_pro_mve$align_res$nu.aligned, res_pro_mcd$align_res$nu.aligned)
 rownames(int_tab) <- c("Female", "Male")
-colnames(int_tab) <- rep(paste0("x", 1:3), 7)
+colnames(int_tab) <- rep(paste0("x", 1:3), 8)
 align_param <- list(`factor loadings` = ld_tab, `intercepts` = int_tab)
 
 ## factor means and factor variances
 m_tab <- rbind(res1$pars[,1],
+               res_adlp$pars[,1],
                res_mve$align_res$pars[,1],
                res_mcd$align_res$pars[,1],
                res_outmve$align_res$pars[,1],
@@ -111,6 +111,7 @@ m_tab <- rbind(res1$pars[,1],
                res_pro_mve$align_res$pars[,1],
                res_pro_mcd$align_res$pars[,1])
 var_tab <- rbind(res1$pars[,2],
+                 res_adlp$pars[,2],
                  res_mve$align_res$pars[,2],
                  res_mcd$align_res$pars[,2],
                  res_outmve$align_res$pars[,2],
@@ -120,6 +121,7 @@ var_tab <- rbind(res1$pars[,2],
 m_var_tab <- cbind(m_tab, var_tab)
 colnames(m_var_tab) <- rep(c("Female", "Male"),2)
 rownames(m_var_tab) <- c("Original Data",
+                         "Modified Data",
                          "MD with MVE",
                          "MD with MCD",
                          "PMVE (random)",
